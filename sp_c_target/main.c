546a40b97145333ba3282733c680e09a788a5e2d
lovejavaee <lovejavaee@126.com>
2015-05-02 22:53:46 +0800

    add a new target

---
+++ sp_c_target/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sp_c_target
+//
+//  Created by willson on 5/2/15.
+//  Copyright (c) 2015 willson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
