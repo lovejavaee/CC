38f4acdec6a6a65cd377d2a89d8d18b16d65e0f8
willson <willson@willsons-Workbook.local>
2015-05-04 22:29:06 +0800

    chain with points

--- src/points/point3.c
+++ src/points/point3.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
-int main()
+
+struct Student
+    {
+    	char   name[10];
+    	char   number[8];
+    	char   sex;
+    	int    age;
+    	double score;
+    	struct Student * next;
+    };
+
+int main(int argc, char *argv[])
 {
-	int a = 10, *ap;
-	ap = &a;
-	printf("a的地址:%p\n", ap);
-	ap += 1;
-	printf("a的地址:%p\n", ap);
-	ap += 1;
-	printf("a的地址:%p\n", ap);
-	ap += 1;
-	printf("a的地址:%p\n", ap);
-	ap += 1;
-	printf("a的地址:%p\n", ap);
-	ap += 1;
-	printf("a的地址:%p\n", ap);
-	ap += 1;
-	printf("a的地址:%p\n", ap);
-	long int b = 11, *bp;
-	bp = &b;
-	bp += 1;
-	printf("b的地址:%p\n", &b);
-	printf("b的地址:%p\n", bp);
-	bp += 1;
-	printf("b的地址:%p\n", bp);
-	int c = 10, *cp;
-	cp = &c;
-	printf("c的地址:%p\n", cp);
-	cp += 1;
-	printf("c的地址:%p\n", cp);
-	char chr = 'A';
-	printf("r的地址:%p\n", &chr);
-	char chr1 = 'A';
-	printf("r的地址:%p\n", &chr1);
+    struct Student LiMing = {"LiMing", "SG11228", 'M', 23, 98.5, NULL};
+    struct Student XiaoLi = {"XiaoLi", "SG11229", 'F', 22, 90, NULL};
+    struct Student * sp = &LiMing;
+    LiMing.next = &XiaoLi;
+
+    printf("%s\t%s\t%s\t%s\t%s\n","Name","Number","Age","Sex","Score");
+    printf("---------------------------------------\n");
+    printf("%s\t%s\t%d\t%c\t%.2f\n", sp->name, sp->number, sp->age, sp->sex, sp->score);
+    printf("%s\t%s\t%d\t%c\t%.2f\n", sp->next->name, sp->next->number, sp->next->age, sp->next->sex, sp->next->score);
+/*
+    sp = sp->next;
+    printf("%s\t%s\t%d\t%c\t%.2f\n",sp->name,sp->number,sp->age,sp->sex,sp->score);
+*/
+    return 0;
 }
+
